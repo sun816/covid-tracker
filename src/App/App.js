64d8f0d5e0ce0469c5f9bc9ea69eb20c9ecd1fe3
sun816 <sunjoypie@gmail.com>
2022-01-13 09:27:17 +0800

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -68,7 +68,7 @@ const App = () => {
 		<div className='app'>
 			<div className='app__left'>
 				<div className='app__header'>
-					<h1>COVID-19 Tracker</h1>
+					<h1>COVID-19 Daily sjp</h1>
 					<FormControl className='app__dropdown'>
 						<Select variant='outlined' value={country} onChange={onCountryChange}>
 							<MenuItem value='worldwide'>Worldwide</MenuItem>
